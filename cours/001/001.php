d26943071c4641a4f2ea9e235003ac21f905aa5e
Mr Roux <XenocodeRCE@users.noreply.github.com>
2023-11-22 14:08:42 +0100

    Update 001.php

--- cours/001/001.php
+++ cours/001/001.php
@@ -1,11 +1,11 @@
-<?php echo make_titre1("Chapitre 1 :Introduction");?>
-<?php echo make_paragraph("Une description du contexte et des enjeux.");?>
+echo make_titre1("Chapitre 1 :Introduction");
+echo make_paragraph("Une description du contexte et des enjeux.");
 ```
-<?php echo make_quote("Une citation");?>
-<?php echo make_paragraph("Un paragraphe.");?>
-<?php echo make_titre2("Un titre du chapitre");?>
-<?php echo make_paragraph("<a href='#'>J'insiste</a> pour dire cela.");?>
-<?php echo make_titre2("Un autre titre du chapitre");?>
-<?php echo make_paragraph("<a href='#'>J'insiste</a> encore une fois.");?>
-<?php echo make_image("https://rainerebert.files.wordpress.com/2015/06/schoolofathens.jpg", "L'École d'Athènes, peinture de Raphaël.");?>
-<?php echo make_author("https://i.imgur.com/IeVCP3e.png", "Aristote", "384-322 av. J.-C.", "Bonjour, moi c'est Aristote de Stagire, une petite cité de Chalcidique située sur le golfe Strymoniquen au nord de la Grèce.");?>
+echo make_quote("Une citation");
+echo make_paragraph("Un paragraphe.");
+echo make_titre2("Un titre du chapitre");
+echo make_paragraph("<a href='#'>J'insiste</a> pour dire cela.");
+echo make_titre2("Un autre titre du chapitre");
+echo make_paragraph("<a href='#'>J'insiste</a> encore une fois.");
+echo make_image("https://rainerebert.files.wordpress.com/2015/06/schoolofathens.jpg", "L'École d'Athènes, peinture de Raphaël.");
+echo make_author("https://i.imgur.com/IeVCP3e.png", "Aristote", "384-322 av. J.-C.", "Bonjour, moi c'est Aristote de Stagire, une petite cité de Chalcidique située sur le golfe Strymoniquen au nord de la Grèce.");
